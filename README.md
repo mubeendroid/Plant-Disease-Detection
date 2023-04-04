bb00e7037f1b3c7f5a30f3707e4e104bbff7e596
Mubeen Ul Alam <42033383+mubeendroid@users.noreply.github.com>
2023-04-04 19:31:29 +0600

    Added dataset and updated project files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Plant-Disease-Detection
+
+This python code implements a model using VGGNet architecture of Convolutional Neural Network which was trained by a 
+dataset of images in order to identify plant diseases.
+
+The following picture provides a visual representation of the implemented model.
+<div align="center">
+   <img src="model_visualization.png" width="500" />
+</div>
--- plant_disease_detection.py
+++ plant_disease_detection.py
@@ -1,63 +1,65 @@
-#####################Importing modules#########################
+"""
+Author: Mubeen Ul Alam
+Website: https://mubeendroid.com
+"""
+
+"""Importing modules"""
 import numpy as np
 import pickle
 import cv2
 from os import listdir
 from sklearn.preprocessing import LabelBinarizer
 from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation, Flatten, Dropout, Dense
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
-from keras.preprocessing import image
-from keras.preprocessing.image import img_to_array
-from sklearn.preprocessing import MultiLabelBinarizer
+from tensorflow.keras.utils import img_to_array
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-#####################Getting uploaded dataset from google drive#########################
-from google.colab import drive
-drive.mount('/content/drive')
 
-#####################Defining parameters#########################
+"""Defining parameters"""
 EPOCHS = 50
 INIT_LR = 1e-3
 BS = 32
 default_image_size = tuple((256, 256))
 image_size = 0
-directory_root = '../content/drive/My Drive/dataset/color'
-width=256
-height=256
-depth=3
+directory_root = 'dataset'
+width = 256
+height = 256
+depth = 3
 
-#####################Coverting image to array#########################
+
+"""Converting image to array"""
 def convert_image_to_array(image_dir):
     try:
         image = cv2.imread(image_dir)
-        if image is not None :
+        if image is not None:
             image = cv2.resize(image, default_image_size)
             return img_to_array(image)
-        else :
+        else:
             return np.array([])
     except Exception as e:
         print(f"Error : {e}")
         return None
 
-#####################Appending images from dataset to list#########################
+
+"""Appending images from dataset to list"""
 image_list, label_list = [], []
 try:
     print("[INFO] Loading images ...")
     root_dir = listdir(directory_root)
 
     for plant_folder in root_dir:
-        print(plant_folder)
+        print(f'Folder name: {plant_folder}')
         plant_disease_image_list = listdir(f"{directory_root}/{plant_folder}/")
         for image in plant_disease_image_list[:100]:
             image_directory = f"{directory_root}/{plant_folder}/{image}"
-            if image_directory.endswith(".jpg") == True or image_directory.endswith(".JPG") == True:
+            if image_directory.endswith(".jpg") is True or image_directory.endswith(".JPG") is True:
                 image_list.append(convert_image_to_array(image_directory))
                 label_list.append(plant_folder)
 
@@ -65,42 +67,48 @@ try:
 except Exception as e:
     print(f"Error : {e}")
 
-#####################Printing image size#########################
-image_size = len(image_list)
-print(image_size)
-print(len(label_list))
 
-#####################Converting class labels into binary label#########################
+"""Printing image size"""
+image_size = len(image_list)
+print(f'Total images: {image_size}')
+
+
+"""Converting class labels into binary label"""
 label_binarizer = LabelBinarizer()
 image_labels = label_binarizer.fit_transform(label_list)
-pickle.dump(label_binarizer,open('label_transform.pkl', 'wb'))
+pickle.dump(label_binarizer, open('label_transform.pkl', 'wb'))
 n_classes = len(label_binarizer.classes_)
 
-#####################Printing class labels#########################
-print(label_binarizer.classes_)
 
-#####################Converting image list into numpy array#########################
+"""Printing class labels"""
+print(f'Disease class labels: {label_binarizer.classes_}')
+
+
+"""Converting image list into numpy array"""
 np_image_list = np.array(image_list, dtype=np.float16) / 225.0
 
-#####################Splitting dataset into training testing set#########################
-print("[INFO] Spliting data to train, test")
-x_train, x_test, y_train, y_test = train_test_split(np_image_list, image_labels, test_size=0.2, random_state = 42)
 
-#####################Image augmentation#########################
+"""Splitting dataset into training testing set"""
+print("[INFO] Spliting data to train, test")
+x_train, x_test, y_train, y_test = train_test_split(np_image_list, image_labels, test_size=0.2, random_state=42)
+
+
+"""Image augmentation"""
 aug = ImageDataGenerator(
     rotation_range=25, width_shift_range=0.1,
     height_shift_range=0.1, shear_range=0.2,
-    zoom_range=0.2,horizontal_flip=True,
+    zoom_range=0.2, horizontal_flip=True,
     fill_mode="nearest")
 
-#####################Building the model#########################
+
+"""Building the model"""
 model = Sequential()
 inputShape = (height, width, depth)
 chanDim = -1
 if K.image_data_format() == "channels_first":
     inputShape = (depth, height, width)
     chanDim = 1
-model.add(Conv2D(32, (3, 3), padding="same",input_shape=inputShape))
+model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
 model.add(Activation("relu"))
 model.add(BatchNormalization(axis=chanDim))
 model.add(MaxPooling2D(pool_size=(3, 3)))
@@ -128,52 +136,51 @@ model.add(BatchNormalization())
 model.add(Dropout(0.5))
 model.add(Dense(n_classes))
 model.add(Activation("softmax"))
-
 model.summary()
 
-#####################Optimizing the model#########################
+
+"""Optimizing the model"""
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-# distribution
-model.compile(loss="categorical_crossentropy", optimizer=opt,metrics=["accuracy"])
-# train the network
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 print("[INFO] training network...")
 
-#####################Training the model#########################
-history = model.fit_generator(
+
+"""Training the model"""
+history = model.fit(
     aug.flow(x_train, y_train, batch_size=BS),
     validation_data=(x_test, y_test),
     steps_per_epoch=len(x_train) // BS,
     epochs=EPOCHS, verbose=1
-    )
+)
+print(history.history)
 
-#####################generating graphs#########################
-acc = history.history['acc']
-val_acc = history.history['val_acc']
+
+"""Calculating accuracy"""
+print("[INFO] Calculating model accuracy")
+scores = model.evaluate(x_test, y_test)
+print(f"Test Accuracy: {scores[1] * 100}")
+
+
+"""Generating graphs"""
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs = range(1, len(acc) + 1)
-#Train and validation accuracy
 plt.plot(epochs, acc, 'b', label='Training accurarcy')
 plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
 plt.title('Training and Validation accurarcy')
 plt.legend()
-
 plt.figure()
-#Train and validation loss
 plt.plot(epochs, loss, 'b', label='Training loss')
 plt.plot(epochs, val_loss, 'r', label='Validation loss')
 plt.title('Training and Validation loss')
 plt.legend()
 plt.show()
 
-#####################Calculating accuracy#########################
-print("[INFO] Calculating model accuracy")
-scores = model.evaluate(x_test, y_test)
-print(f"Test Accuracy: {scores[1]*100}")
 
-#####################Saving model for further usage#########################
+"""Saving model for further usage"""
 print("[INFO] Saving model...")
-pickle.dump(model,open('cnn_model.pkl', 'wb'))
-
-pickle.dump(history,open('history.pkl', 'wb'))
-pickle.dump(label_binarizer,open('label_transform.pkl', 'wb'))
+pickle.dump(model, open('cnn_model.pkl', 'wb'))
+pickle.dump(history, open('history.pkl', 'wb'))
+pickle.dump(label_binarizer, open('label_transform.pkl', 'wb'))
